d723869e4d4a3c8207cb489e1f2891d41e65b9c1
DESKTOP-UD9E914\도희 <155641@naver.com>
2022-02-15 22:33:11 +0900

    파일 이름, sign up box  위치 수정

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,21 +1,53 @@
 
+function Validation(){
+    const RegExp = /^[a-zA-Z0-9]{4,12}$/;
+    const ERegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+    const id = document.querySelector("#id");
+    const pw = document.querySelector("#pw");
+    const RPw = document.querySelector("#r-pw");
 
 
-function create_id () {
 
-const id = document.querySelector('#id');
-const pw = document.querySelector('#pw');
-const Rpw = document.querySelector('r-pw');
-
-    if(id.value == "" || pw.value == "" || Rpw.value == "") {
-        alert ("회원가입을 할 수 없습니다.")
+    if(ERegExp.value == ''){ 
+        alert("이메일을 입력해주세요.");
+        return false;
     }
-    else{
-        if (pw.value !== Rpw.value) {
-            alert("비밀번호를 확인해 주세요")
-        }
-        else {
-            location.href = 'login-box.html';
-        }
+    
+    if(!ERegExp.test(id.value)){
+        alert("올바른 이메일 형식이 아닙니다.");
+        return false;
     }
+    
+
+    if(id.value==''){
+        alert("ID를 입력해주세요.");
+        return false;
+    }
+    if(!RegExp.test(id.value)){ 
+        alert("ID는 4~12자의 영문 대소문자와 숫자로만 입력하여 주세요.");        
+        return false;
+    }
+    
+
+    if(pw.value==''){
+        alert("Password를 입력해주세요.");
+        return false;
+    }
+    if(!RegExp.test(pw.value)){ 
+        alert("Password는 4~12자의 영문 대소문자와 숫자로만 입력하여 주세요.");
+        return false;
+    }
+    if(pw.value==id.value){ 
+        alert("Password는 ID와 동일하면 안됩니다.");
+        return false;
+    }
+    
+    if(RPw.value!=pw.value){ 
+        alert("비밀번호가 틀립니다. 다시 확인하여 입력해주세요.");
+        return false;
+    }
+    
 }
+
+
